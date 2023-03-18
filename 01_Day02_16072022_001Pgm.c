4c504efd5fc51dc975ef5cede1697dd123987384
Vishnu_Kannancher <128255646+VishnuKannanchery@users.noreply.github.com>
2023-03-19 01:15:16 +0530

    Add files via upload

---
+++ 01_Day02_16072022_001Pgm.c
@@ -0,0 +1,51 @@
+/*
+===============================================================================
+ Name        : 01_Day02_16072022_001Pgm
+ Date	     : 16-07-2022
+ Author      : Vishnu_Kannanchery
+ Version     :
+ Description : Demonstration of escape sequences
+
+
+===============================================================================
+*/
+
+/* Includes ------------------------------------------------------------------*/
+#include <stdio.h>
+#include <unistd.h>
+/* Private includes ----------------------------------------------------------*/
+
+/* Private typedef -----------------------------------------------------------*/
+
+/* Private define ------------------------------------------------------------*/
+
+/* Private macro -------------------------------------------------------------*/
+
+/* Private variables ---------------------------------------------------------*/
+
+/* Private function prototypes -----------------------------------------------*/
+
+/* Private user code ---------------------------------------------------------*/
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+
+  /* USER CODE BEGIN  */
+	printf("Welcome to Embedded System world \n");
+	printf("Testing \r");
+	printf("Hello ");
+	//printf("Hello \n");
+  /* USER CODE END  */
+
+  /* End of Application entry point */
+}
+
+
+
+
+
+
